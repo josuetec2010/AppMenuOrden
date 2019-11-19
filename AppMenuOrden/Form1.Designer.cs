26d6ee5c832d155f2849a1f127c8d204f92a329a
Alfredo Rueda <josuetec2003@icloud.com>
2019-11-19 16:49:19 -0600

    ejemplo de ordenes

---
+++ AppMenuOrden/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace AppMenuOrden
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.flowPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1482, 119);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.splitContainer1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 119);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1482, 524);
+            this.panel2.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.White;
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox3);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.flowPanel);
+            this.splitContainer1.Size = new System.Drawing.Size(1482, 524);
+            this.splitContainer1.SplitterDistance = 472;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // flowPanel
+            // 
+            this.flowPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowPanel.Location = new System.Drawing.Point(0, 0);
+            this.flowPanel.Name = "flowPanel";
+            this.flowPanel.Size = new System.Drawing.Size(1006, 524);
+            this.flowPanel.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.GridColor = System.Drawing.Color.White;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(472, 378);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.DataGridView1_DoubleClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Descripcion";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Precio";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cantidad";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Total linea";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 130;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox1.Location = new System.Drawing.Point(237, 392);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 37);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox2.Location = new System.Drawing.Point(237, 435);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(223, 37);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox3.Location = new System.Drawing.Point(237, 478);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(223, 37);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 59);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Quitar de la orden";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1482, 643);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.FlowLayoutPanel flowPanel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AppMenuOrden/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace AppMenuOrden
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<int, KeyValuePair<string, string>> datos;
+        public Form1()
+        {
+            datos = new Dictionary<int, KeyValuePair<string, string>>();
+
+            // simular datos de la base de datos
+            datos.Add(1, new KeyValuePair<string, string>("Papas sencillas", "20.00"));
+            datos.Add(2, new KeyValuePair<string, string>("Papas grandes", "30.00"));
+            datos.Add(3, new KeyValuePair<string, string>("Hamburguesa sencilla", "40.00"));
+            datos.Add(4, new KeyValuePair<string, string>("Coca-Cola 1/2 Lt", "25.00"));
+            datos.Add(5, new KeyValuePair<string, string>("Tacos al pastor", "35.00"));
+            datos.Add(6, new KeyValuePair<string, string>("Orden Enchiladas 3 U", "60.00"));
+            datos.Add(7, new KeyValuePair<string, string>("Pupusa Mixta Unidad", "25.00"));
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var dato in datos)
+            {
+                Button btn = new Button()
+                {
+                    Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point, 0),
+                    ForeColor = Color.RoyalBlue,
+                    Size = new Size(287, 104),
+                    Tag = dato.Key,
+                    Text = $"{dato.Value.Key}\n{dato.Value.Value}",
+                    UseVisualStyleBackColor = true
+                };
+
+                btn.Click += btnMenu_Click;
+
+                flowPanel.Controls.Add(btn);
+            }
+        }
+
+
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            Button botoncito = sender as Button;
+            int id = Convert.ToInt32(botoncito.Tag.ToString());
+
+            // consultar a la base de datos con el id de la linea anterior
+            var prod = datos.Where(x => x.Key == id).First();
+
+            //var prod = (from p in ctx.menu
+            //            where p.id = id).firstordefault();
+
+            dataGridView1.Rows.Add(prod.Value.Key, prod.Value.Value, 1, prod.Value.Value);
+            calcularTotales();
+        }
+
+        void calcularTotales()
+        {
+            double total = 0;
+            foreach (DataGridViewRow fila in dataGridView1.Rows)
+            {
+                total += Convert.ToDouble(fila.Cells[3].Value);
+            }
+
+            textBox1.Text = total.ToString();
+            textBox2.Text = (total * 0.15).ToString();
+            textBox3.Text = (total + (total * 0.15)).ToString();
+        }
+
+        private void DataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                int cant = Convert.ToInt32( Interaction.InputBox("Ingrese la cantidad: ") );
+                double precio = Convert.ToDouble(dataGridView1.CurrentRow.Cells[1].Value);
+                dataGridView1.CurrentRow.Cells[2].Value = cant;
+                dataGridView1.CurrentRow.Cells[3].Value = cant * precio;
+
+                calcularTotales();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int indice = dataGridView1.CurrentRow.Index;
+            dataGridView1.Rows.RemoveAt(indice);
+
+            calcularTotales();
+        }
+    }
+}
